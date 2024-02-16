edb7d51fab13b314a29bb9aa70a73d7db7309709
FelixArg <62147533+FelixArg@users.noreply.github.com>
2024-02-16 20:50:37 +0300

    Create Aho-Corasick.cpp

---
+++ Strings/Aho-Corasick.cpp
@@ -0,0 +1,76 @@
+struct Aho_Corasick{
+    const static int al = 26;
+    struct Node{
+        Node* to[al];
+        Node* go[al];
+        Node* link = nullptr;
+        char pred_ch;
+        Node* pred = nullptr;
+        bool vis = 0;
+        Node(char _pred_ch, Node* _pred): pred_ch(_pred_ch), pred(_pred){
+            memset(to, 0, sizeof(to));
+            memset(go, 0, sizeof(go));
+        }
+    };
+
+    Node* root = nullptr;
+    void add(const string& s){
+        Node* cur = root;
+        for (char ch : s){
+            if (!cur->to[ch - 'a']){
+                cur->to[ch - 'a'] = new Node(ch, cur);
+            }
+            cur = cur->to[ch - 'a'];
+        }
+    }
+
+    Node* find(const string& s){
+        Node* cur = root;
+        for (char ch : s){
+            if (!cur->to[ch - 'a']){
+                return nullptr;
+            }
+            cur = cur->to[ch - 'a'];
+        }
+        return cur;
+    }
+
+    //Node* go(Node* v, char ch);
+
+    Node* link(Node* v){
+        if (!v->link){
+            if (v == root || v->pred == root){
+                v->link = root;
+            }
+            else{
+                v->link = go(link(v->pred), v->pred_ch);
+            }
+        }
+        return v->link;
+    }
+
+    Node* go(Node* v, char ch){
+        if (!v->go[ch - 'a']){
+            if (v->to[ch - 'a']){
+                v->go[ch - 'a'] = v->to[ch - 'a'];
+            }
+            else if (v == root){
+                v->go[ch - 'a'] = root;
+            }
+            else{
+                v->go[ch - 'a'] = go(link(v), ch);
+            }
+        }
+        return v->go[ch - 'a'];
+    }
+
+    void dfs(Node* v){
+        if (v->vis){
+            return;
+        }
+        v->vis = 1;
+        dfs(link(v));
+    }
+
+    Aho_Corasick(): root(new Node(0, nullptr)){}
+};
