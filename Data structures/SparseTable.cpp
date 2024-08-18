4632e3e49248c6c8baf4a37d395cc2fdb25f3a94
FelixArg <62147533+FelixArg@users.noreply.github.com>
2024-08-18 17:35:42 +0300

    Update SparseTable.cpp

--- Data structures/SparseTable.cpp
+++ Data structures/SparseTable.cpp
@@ -5,18 +5,18 @@ struct SparseTable{
 	void build(vector<T> &a) {
 		n = a.size();
 		r = 32 - __builtin_clz(n);
-		t.resize(n + 1, vector<T>(r));
-		for(int i = 1; i <= n; ++i) t[i][0] = a[i - 1];
+		t.resize(r, vector<T>(n + 1));
+		for(int i = 1; i <= n; ++i) t[0][i] = a[i - 1];
 		for(int k = 1; k < r; ++k) {
 			for(int i = 1; i + (1 << k) - 1 <= n; ++i) {
-				t[i][k] = f(t[i][k - 1], t[i + (1 << (k - 1))][k - 1]);
+				t[k][i] = f(t[k - 1][i], t[k - 1][i + (1 << (k - 1))]);
 			}
 		}
 	}
 
 	T query(int l, int r) {
 		int k = 31 - __builtin_clz(r - l + 1);
-		return f(t[l][k], t[r - (1 << k) + 1][k]);
+		return f(t[k][l], t[k][r - (1 << k) + 1]);
 	}
 
 	inline T f(T x, T y){
